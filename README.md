35c1aa7bb80cb055ab0034d167df141547a6bfbd
Prapti Sharma <praptisharma282003@gmail.com>
2025-07-11 00:50:32 +0530

    structs

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # miniserve-rs
 A minimal HTTP server built in Rust from scratch to explore low-level networking concepts, request parsing, and response handling. Inspired by how modern web servers work under the hood.
+
+## Thoery
+- Http/1.1
+- it is a l7 protocol. 
+- sent over tcp
+- message based
+
+- request: GET/search?name=abc HTTP/1.1 Accept:text/html
+- response: HTTP/1.1 200 OK Content-Type: text/html 
+
+- in the architecture, we have 3 main components:
+
+tcp listener
+http parser
+handler
+
+all of these will be running on a single thread
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
-    println!("Hello, world!");
+    let miniserver = Miniserver::new("127.0.1:8080");//new is an associated function of Miniserver
+    miniserver.run();
+
+    // miniserver here is a struct
+    struct Miniserver {
+        addr: String,
+    }
+
+    impl Miniserver{
+        fn new(addr: String) -> Self {
+            Self {
+                addr
+            }
+        }
+        // fn new here is an associated function of Miniserver
+
+        fn run(self) {
+            println!("Running server at {}", self.addr);
+        }
+        //self in rust works as this in other languages, it holds the ownership of the struct
+        // we will use self instead of &self because we want to take ownership of the struct
+    }
 }
