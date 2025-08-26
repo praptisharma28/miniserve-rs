cbd06362f8e327702a9cf95624c0a7f5c3a5471d
Prapti Sharma <praptisharma282003@gmail.com>
2025-08-26 14:56:23 +0000

    feat: first connection established wohoo

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,9 @@
-use http::Method;
-use http::Request;
-use miniserver::Miniserver;
-
-mod http;
-mod miniserver;
+use std::net::TcpListener;
 
 fn main() {
-    // let string_slice = &string[10..]; // this means give everything after the 10th byte
-    // let string = String::from("127.0.0.1:8080");//new is an associated function of Miniserver
-    // let string_borrow: &str = &string;
-    // let string_literal = "1234";
-    //normal string can shrink and explan dynamically at run time but slice strings are immutable
-
-    // dbg!(&string);
-    // dbg!(string_slice);
-    // dbg!(string_borrow);
-    // dbg!(string_literal);
-
-    let miniserver = Miniserver::new("127.0.0.1:8080".to_string());
-    miniserver.run();
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    for stream in listener.incoming(){
+        let stream = stream.unwrap();
+        println!("connection established");
+    }
 }
-// miniserver here is a struct
