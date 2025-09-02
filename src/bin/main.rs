6bd83fb2f95537db858061b24fc574b6927611f1
Prapti Sharma <praptisharma282003@gmail.com>
2025-09-02 21:28:00 +0000

    feat: signaling to the threads

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -9,7 +9,7 @@ use miniserve_rs::ThreadPool;
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     let pool = ThreadPool::new(4);
-    for stream in listener.incoming(){
+    for stream in listener.incoming().take(2){
         let stream = stream.unwrap(); //returns result type
         pool.execute( || {
             handle_connection(stream);
